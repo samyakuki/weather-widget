a9a85b024c14c61cc0eb7304d3a31b8dcad815e4
Your Name <your@email.com>
2025-07-01 17:41:35 +0530

    weatherApi

--- weather-widget-app/src/searchBox.jsx
+++ weather-widget-app/src/searchBox.jsx
@@ -6,6 +6,24 @@ import { useState } from 'react';
 
 export default function SearchBox(){
     let [city,setCity]=useState("");
+
+    const Api_URL="https://api.openweathermap.org/data/2.5/weather";
+    const Api_KEY="8fb1125867086c4aff4abd39bcd549e7";
+
+    let getWeaterInfo=async ()=>{
+        const res = await fetch(`${Api_URL}?q=${city}&appid=${Api_KEY}&units=metric`);
+        const jsonResponse = await res.json();
+        let result = {
+             temp: jsonResponse.main.temp,
+             tempMin: jsonResponse.main.temp_min,
+             tempMax: jsonResponse.main.temp_max,
+             humidity: jsonResponse.main.humidity,
+             feelsLike: jsonResponse.main.feels_like,
+             weather: jsonResponse.weather[0].description,
+        };
+        console.log(result);
+
+    }
     let handleChange= (evt)=>{
         setCity(evt.target.value);
     }
@@ -13,6 +31,7 @@ export default function SearchBox(){
         evt.preventDefault();
         console.log(city);
         setCity("");
+        getWeaterInfo();
     }
     return <>
     <h3>Search for the Weather</h3>
